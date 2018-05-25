109b05298c3a4a5950fe1852c4d643add28830ff
Bastien91580 <Bastien91580@yahoo.fr>
2018-05-25 16:30:20 +0200

    push 1/6

---
+++ nav.php
@@ -0,0 +1,63 @@
+<!-- Navigation-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <a class="navbar-brand" href="dash.php">BeYourBet</a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+          <a class="nav-link" href="dash.php">
+            <i class="oi oi-dashboard"></i>
+            <span class="nav-link-text">&nbsp;Dashboard</span>
+          </a>
+        </li>
+
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+          <a class="nav-link" href="match.php">
+            <i class="oi oi-monitor"></i>
+            <span class="nav-link-text">&nbsp;Matchs</span>
+          </a>
+        </li>
+
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+          <a class="nav-link" href="team.php">
+            <i class="oi oi-people"></i>
+            <span class="nav-link-text">&nbsp;Equipe</span>
+          </a>
+        </li>
+
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+          <a class="nav-link" href="event.php">
+            <i class="oi oi-list"></i>
+            <span class="nav-link-text">&nbsp;Competition</span>
+          </a>
+        </li>
+
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+          <a class="nav-link" href="user.php">
+            <i class="oi oi-person"></i>
+            <span class="nav-link-text">&nbsp;Utilisateurs</span>
+          </a>
+        </li>
+          
+        
+        
+
+      </ul>
+      <ul class="navbar-nav sidenav-toggler">
+        <li class="nav-item">
+          <a class="nav-link text-center" id="sidenavToggler">
+            <i class="fa fa-fw fa-angle-left"></i>
+          </a>
+        </li>
+      </ul>
+        
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="../deconnexion.php">
+            <i class="fa fa-fw fa-sign-out"></i>Logout</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
